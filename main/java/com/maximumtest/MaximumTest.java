bc3d3273cececaf9428cd93576f7d1adc2caa725
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-11 13:56:58 +0530

    added : Welcome file added

---
+++ main/java/com/maximumtest/MaximumTest.java
@@ -0,0 +1,7 @@
+package com.maximumtest;
+/*@Description:- To find the maximum value among three objects.*/
+public class MaximumTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome in Generics concept");
+    }
+}
---
+++ test/java/com/genericprog/MaximumJunitTest.java
@@ -0,0 +1,11 @@
+package com.genericprog;
+
+import com.maximumtest.MaximumTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaximumJunitTest {
+
+
+
+}
